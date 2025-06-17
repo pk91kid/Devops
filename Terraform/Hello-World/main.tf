fd92230caa23f0d88cd540d17177a0354e5e0279
Anoop PK <Anoop@localhost.localdomain>
2025-06-17 12:20:22 +0530

    added hello-world

---
+++ Terraform/Hello-World/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "null" {}
+
+resource "null_resource" "hello" {
+  provisioner "local-exec" {
+    command = "echo hello-world"
+  }
+}
+
